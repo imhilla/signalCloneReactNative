285e05a66eae78d46a3be5d9a56721e7b7eeac43
imhilla <hillaryodhiambo@gmail.com>
2021-03-24 07:55:53 +0300

    add navigation container

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCmfSwDjhpjqJplshIAAwtKueURqwYR22s",
+  authDomain: "signal-c46d8.firebaseapp.com",
+  projectId: "signal-c46d8",
+  storageBucket: "signal-c46d8.appspot.com",
+  messagingSenderId: "235011005339",
+  appId: "1:235011005339:web:cb9ef0d4bb0513d3c81f3e"
+};
